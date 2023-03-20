4bb9b81996d29afe4734c6c661028975683a69fb
Md Nahid Prawez <nahid.prawez@gmail.com>
2023-03-21 00:18:45 +0530

    Ass1 of Module 7 routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,35 @@ import { EmployeeComponent } from './employee/employee.component';
 import {FormsModule} from '@angular/forms';
 import { NewemployeeComponent } from './newemployee/newemployee.component';
 import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
+import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+const routes: Routes = [
+   {path : "", component : HomeComponent},
+   {path : "contact", component : ContactComponent},
+   {path : "products", component : ProductComponent},
+   {path : "**", component : NotFoundComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
     ColorDirective,
-    NewemployeeComponent
+    NewemployeeComponent,
+    HomeComponent,
+    ProductComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
